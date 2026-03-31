93f38253eb1992677cf097cc4fccbdeb6ed803ed
Poornima-Jayapalan <database.coder@gmail.com>
2026-03-30 23:04:58 -0400

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,5 +1,8 @@
 from pathlib import Path
 
 MODEL = "llama3.1:8b"
+
+# Repo root
 BASE_DIR = Path(__file__).resolve().parent.parent
 VECTORSTORE_DIR = BASE_DIR / "vectorstores"
+VECTORSTORE_DIR.mkdir(exist_ok=True)
