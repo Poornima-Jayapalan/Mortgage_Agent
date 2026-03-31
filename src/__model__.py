3c22ca5e381953d8e44f4fce41f31355513ee5d6
Poornima-Jayapalan <database.coder@gmail.com>
2026-03-30 22:57:49 -0400

    Delete src/__model__.py

--- src/__model__.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-from langchain_community.chat_models import ChatOllama
-from .config import MODEL
-
-@st.cache_resource
-def get_model():
-    return ChatOllama(model=MODEL, temperature=0)
